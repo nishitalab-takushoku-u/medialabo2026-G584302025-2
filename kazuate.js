8206273993100fd5742af861ad3dbb2dfb3b1724
G584302025 <258430@st.takushoku-u.ac.jp>
2026-06-25 16:44:10 +0900

    数当てゲーム

--- kazuate.js
+++ kazuate.js
@@ -11,20 +11,42 @@ let kaisu = 0;
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
+  kaisu +=1;
+  p = document.querySelector('span#kaisu');
+  p.textContent = kaisu;
   // ここから: 予想回数を1増やして，span#kaisu 要素のテキストを更新
 
   // ここまで: 予想回数を1増やして，span#kaisu 要素のテキストを更新
-  
+  let i = document.querySelector('input[name="kazu"]');
+  i.getAttribute('name');
   // ここから: テキストボックスに指定された数値を yoso に代入する
-  let yoso;
+  let yoso =Number(i.value);
+  let i2 = document.querySelector('span#yosou2');
+  i2.textContent = yoso ;
   // ここまで: テキストボックスに指定された数値を yoso に代入する
-  
   // ここから: 正解判定する
   // 　　　　  正解/不正解のときのメッセージを表示する
-
+  p2 = document.querySelector('p#result');
+  if(kaisu<3){
+    if(kotae === yoso){
+      p2.textContent = '正解です。おめでとう!';
+    }else if(kotae < yoso){
+      p2.textContent = 'まちがい。答えはもっと小さいですよ。';
+    }else if(kotae > yoso){
+      p2.textContent = 'まちがい。答えはもっと大きいですよ。';
+    }
+  }else if(kaisu === 3){
+    if(kotae === yoso){
+      p2.textContent = '正解です。おめでとう!';
+    }else{
+      p2.textContent = 'まちがい。残念でした、答えは '+ kotae +'です。';
+    }
+  }else{
+    p2.textContent = '答えは '+ kotae +' でした．すでにゲームは終わっています';
+  }
   // ここまで: 正解判定する
 }
-
 // ここから: ボタンを押した時のイベントハンドラとして hantei を登録
-
+b = document.querySelector('button#yosou');
+b.addEventListener('click',hantei);
 // ここまで: ボタンを押した時のイベントハンドラとして hantei を登録
